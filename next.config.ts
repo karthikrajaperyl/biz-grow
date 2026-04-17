8fdd7819c986568d30ec456119bba7b48604735b
Karthikraja <karthikrajaperiyasamy@gmail.com>
2026-04-17 19:06:52 +0530

    changes the config to commit

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -30,6 +31,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: 'export', 
 };
 
 export default nextConfig;
